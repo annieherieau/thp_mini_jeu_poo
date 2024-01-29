fbb468e4af11553da00e49517f327ce4dbc07333
annieherieau <annie.herieau@gmail.com>
2024-01-30 00:18:47 +0100

    humanPlayer created

--- app2.rb
+++ app2.rb
@@ -1,3 +1,11 @@
 require 'bundler'
 Bundler.require
 
+require_relative 'lib/game'
+require_relative 'lib/player'
+
+# Start game : players
+player1 = Player.new("Josiane")
+player2 = Player.new("José")
+annie = HumanPlayer.new('Annie')
+binding.pry
--- lib/player.rb
+++ lib/player.rb
@@ -5,14 +5,11 @@ class Player
   #variables d'instances
   attr_accessor :life_points, :name, :avatar
   
-
-  #variables de classe
- 
   # initialisation
   def initialize(name)
     @name = name
     @life_points = 10
-    @avatar = ['🐶', '🥰', '🦄​', '🐯​', '😁​', "🔥", "🧊", "🍀", '🎲'].sample
+    @avatar = ['🐶', '🦄​', '🐯​', "🔥", "🍀", '🎲', '😸'].sample
   end
 
   # affiche l'état du joueur
@@ -45,3 +42,54 @@ class Player
   end
 end
 
+class HumanPlayer < Player
+  attr_accessor :@weapon_level
+
+  # initialisation
+  def initialize(name)
+    @name = name
+    @life_points = 100
+    @avatar = ['🥰', '😁​', '😇', '😎', '🥳'].sample
+    @weapon_level = 1
+  end
+
+  # affiche l'état du joueur
+  def show_state
+    @life_points > 1 ? s = 's' : s = ''
+    puts "#{@avatar} #{@name} a #{@life_points} point#{s} de vie et une arme de niveau #{@weapon_level}"
+  end
+
+  # calcul des points d'attaque
+  def compute_damage
+    rand(1..6)*weapon_level
+  end
+
+  # chercher une nouvelle arme
+  def search_weapon
+    new_weapon = rand(1..6)
+    puts "Tu as trouvé une arme de niveau #{new_weapon}"
+    if new_weapon > @weapon_level
+      @weapon_level = new_weapon 
+      puts "Youhou ! elle est meilleure que ton arme actuelle : tu la prends."
+    else
+    "M@*#$... elle n'est pas mieux que ton arme actuelle..."
+    end
+  end
+
+  # chercher un pack de points de vie
+  def search_health_pack
+    pack = rand(1..6)
+    case pack
+    when 1
+      puts "Tu n'as rien trouvé... "
+    when 6
+      @life_points += 80
+      puts "Waow, tu as trouvé un pack de +80 points de vie !"
+    else
+      @life_points += 50
+      puts "Bravo, tu as trouvé un pack de +50 points de vie !"
+    end
+    @life_points= 100 if @life_points > 100
+  end
+
+end
