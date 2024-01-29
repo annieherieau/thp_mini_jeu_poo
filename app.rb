5c4727ec6f389fd1285753fc651c4f8d1110dda5
annieherieau <annie.herieau@gmail.com>
2024-01-29 23:48:39 +0100

    app v.01 bis

--- app.rb
+++ app.rb
@@ -4,11 +4,10 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-
+# Start game : players
 player1 = Player.new("Josiane")
 player2 = Player.new("José")
 
-# Start game : players
 puts "À ma droite #{player1.name} #{player1.avatar}"
 puts "À ma gauche #{player2.name} #{player2.avatar}"
 puts "------"
@@ -16,10 +15,12 @@ puts "------"
 # Fighting rounds 
 loop do
   break if player1.life_points.zero? || player1.life_points.zero?
+  
   puts "🤍 🤍 🤍 🤍".center(25, ' ')
   puts "Voici l'état de chaque joueur :"
   player1.show_state
   player2.show_state
+  
   puts "💥 💥 💥 💥".center(25, ' ')
   puts "Passons à la phase d'attaque :"
   player1.attacks(player2)
@@ -31,5 +32,3 @@ end
 player1.life_points.zero? ? winner = player2 : player1
 puts "⭐️ ⭐️ ⭐️ ⭐️".center(25, ' ')
 puts "VAINQUEUR : #{winner.name} #{winner.avatar}"
-
-binding.pry
